aebd9120f75608d11f988e2c3c4411092d3d73ad
Abbas Lamouri <abbas@Abbas-Omen.GENVAC.local>
2021-08-05 13:55:29 -0400

    M

--- project-planner/Components/SingleProject.vue
+++ project-planner/Components/SingleProject.vue
@@ -2,6 +2,11 @@
 <div class="project">
   <div class="actions">
     <h3 @click='showDetails = ! showDetails'>{{ project.title }}</h3>
+    <div class="icons">
+      <span class="material-icons edit">edit</span>
+      <span class="material-icons delete" @click='deleteProject(project)'>delete</span>
+      <span class="material-icons done">done</span>
+    </div>
   </div>
   <div v-if='showDetails' class='details'>
     <p>{{ project.details }}</p>
@@ -22,21 +27,31 @@ export default {
     }
   },
 
-  setup(props) {
+  setup(props, ctx) {
         
     const showDetails = ref(false)
+
+    const deleteProject = (project) => {
+      console.log(project)
+      ctx.emit('delete', project.id)
+    }
 
 
     return {
       showDetails,
-      
+      deleteProject
     }
   }
 
 }
 </script>
 
-<style scoped>
+<style scoped lang='scss'>
+
+$red-color: red;
+$green-color: green;
+$grey-color: #444;
+
 .project {
     margin: 20px auto;
     background: white;
@@ -47,5 +62,45 @@ export default {
   }
   h3 {
     cursor: pointer;
+  }
+
+  .actions {
+    display:flex;
+    align-items: center;
+    justify-content: space-between;
+
+    .material-icons {
+      margin-left: 1rem;
+      cursor:pointer;
+      font-size:150%;
+      
+      &.delete {
+        color: $red-color;
+
+         &:hover {
+          color: lighten($red-color, 20%)
+        }
+      
+      }
+
+      &.edit {
+        color:$grey-color;
+
+          &:hover {
+          color: lighten($grey-color, 20%)
+        }
+      }
+
+      &.done {
+        color:$green-color;
+
+          &:hover {
+          color: lighten($green-color, 20%)
+        }
+      }
+
+      
+
+    }
   }
 </style>
